99c10e506589f56e13245ab0e51f81a011bb0e27
Clayton Williams <worldofclayton@gmail.com>
2018-05-10 11:31:57 -0400

    Key Binds

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ class Window(Frame):
     # Our interval to update (100 ms)
     UPDATE_INTERVAL = 100
 
+    # The canvas to draw our game components on
+    canvas = None
+
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
@@ -39,16 +42,29 @@ class Window(Frame):
         self.master.title("2D Snake")
 
         # Set the window size
-        self.master.minsize(width=600, height=500)
+        self.master.minsize(width=900, height=600)
+
+        # Set a handler for keyboard
+        self.master.bind("<Key>", self.key_pressed)
 
         # And the following can set the window to fill (wrap around) its contents if we don't have the above set
         self.pack(fill=BOTH, expand=1)
+
+        # Create a canvas to draw the game on
+        self.canvas = Canvas(self)
+
+        # Pack the canvas
+        self.canvas.pack(fill=BOTH, expand=1)
 
     # Define an update function for game updating and continue to schedule itself on an interval
     def update(self):
 
         # Reschedule after updating
         self.after(self.UPDATE_INTERVAL, self.update)
+
+    # Called when a key is pressed on the keyboard
+    def key_pressed(self, event):
+        print "pressed", event.keysym
 
 
 # Initialize the program
